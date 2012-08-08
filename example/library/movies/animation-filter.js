40c6e511594edc284c35918ffdfa92bdd8abaf1c
Tobi Reiss <tag+github@basecode.de>
2012-08-08 13:52:22 +0200

    fix Bitmap callback

--- example/library/movies/animation-filter.js
+++ example/library/movies/animation-filter.js
@@ -1,49 +1,43 @@
 // animate second filter only
-new bonsai.Bitmap('assets/redpanda.jpg', {
-  onload: function() {
-    this.attr({
-      y: 10,
-      x: 10,
-      scale: 0.5,
-      filters: [filter.hueRotate(0), filter.invert(0)]
-    });
-    stage.addChild(this);
-    this.animate('3s', {
-      filters: [null, 1]
-    });
-  }
+new bonsai.Bitmap('assets/redpanda.jpg', function() {
+  this.attr({
+    y: 10,
+    x: 10,
+    scale: 0.5,
+    filters: [filter.hueRotate(0), filter.invert(0)]
+  });
+  stage.addChild(this);
+  this.animate('3s', {
+    filters: [null, 1]
+  });
 });
 
 // animate first filter only
-new bonsai.Bitmap('assets/redpanda.jpg', {
-  onload: function() {
-    this.attr({
-      y: 10,
-      x: 150,
-      scale: 0.5,
-      filters: [filter.hueRotate(0), filter.invert(0)]
-    });
-    stage.addChild(this);
-    this.animate('3s', {
-      filters: [360]
-    });
-  }
+new bonsai.Bitmap('assets/redpanda.jpg', function() {
+  this.attr({
+    y: 10,
+    x: 150,
+    scale: 0.5,
+    filters: [filter.hueRotate(0), filter.invert(0)]
+  });
+  stage.addChild(this);
+  this.animate('3s', {
+    filters: [360]
+  });
 });
 
 // animate both
-new bonsai.Bitmap('assets/redpanda.jpg', {
-  onload: function() {
-    this.attr({
-      y: 10,
-      x: 290,
-      scale: 0.5,
-      filters: [filter.hueRotate(0), filter.invert(0)]
-    });
-    stage.addChild(this);
-    this.animate('3s', {
-      filters: [360, 1]
-    });
-  }
+new bonsai.Bitmap('assets/redpanda.jpg', function() {
+  this.attr({
+    y: 10,
+    x: 290,
+    scale: 0.5,
+    filters: [filter.hueRotate(0), filter.invert(0)]
+  });
+  stage.addChild(this);
+  this.animate('3s', {
+    filters: [360, 1]
+  });
 });
 
 Path.rect(200,200,100,100)
