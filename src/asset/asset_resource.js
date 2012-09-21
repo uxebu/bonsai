57a9b58db4f2d162ad38a1d779f94f4798d7eb1e
Tobi Reiss <tag+github@basecode.de>
2012-09-21 13:01:28 +0200

    minor jshint fixes

--- src/asset/asset_resource.js
+++ src/asset/asset_resource.js
@@ -14,25 +14,24 @@ define([
    */
   function AssetResource(param) {
 
-    var i, src, type;
+    var src, type;
 
     if (!param) {
       throw Error('AssetResource needs at least a valid url as parameter.');
     }
 
-    if (typeof param == 'string') {
+    if (typeof param === 'string') {
       src = param;
     } else {
       src = param.src;
       type = param.type;
     }
 
-    if (!src || typeof src != 'string') {
+    if (!src || typeof src !== 'string') {
       throw Error('AssetResource: src parameter invalid: ' + src);
     }
 
     this.src = src;
-
     if (type && typeof type === 'string') {
       this.type = type;
     } else {
@@ -40,13 +39,13 @@ define([
       var re = uri.scheme === 'data' ?
         /^(\w+\/[\w+]+)[;,]/ : // extract mime type from beginning
         /\.([^.]+)$/; // extract file extension
-      this.type = (uri.path.match(re) || [, null])[1];
+      this.type = (uri.path.match(re) || [])[1];
     }
 
     if (!this.type) {
       throw Error('Cannot determine type of resource with src: ' + src);
     }
-  };
+  }
 
   return AssetResource;
 });
