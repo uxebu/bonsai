01fda4a4454afabe2f086b4bda31b54d3ddd0a90
Dustan Kasten <dustan.kasten@gmail.com>
2012-09-22 11:31:36 -0400

    Forking reporters based on environment.

--- test/runner.js
+++ test/runner.js
@@ -7,10 +7,7 @@ define([
   'order!jasmine-matchers.js',
   'order!../lib/jasmine-console/console-runner.js'
 ], function(jasmineCore, jasmineHtml, junitReporter, jasmineHelper, compareReporter) {
-  // exporting this to window because ConsoleReporter is complaining otherwise
-  var console_reporter = window.console_reporter = new jasmine.ConsoleReporter();
   var jasmineEnv = jasmine.getEnv();
-  jasmineEnv.addReporter(console_reporter);
   jasmineEnv.updateInterval = 10000;
 
   this.async = jasmineHelper.async;
@@ -33,16 +30,24 @@ define([
     });
   }
 
-  var trivialReporter = new jasmine.TrivialReporter();
+  var isPhantom = (navigator && navigator.userAgent.indexOf('PhantomJS') > -1);
+  var reporter = isPhantom ? new jasmine.ConsoleReporter() : new jasmine.TrivialReporter();
 
-  jasmineEnv.addReporter(trivialReporter);
-  jasmineEnv.addReporter(new jasmine.JUnitXmlReporter('', true, false));
-  // passing trivialReporter so we can access the individual DOM nodes
-  jasmineEnv.addReporter(new jasmine.CompareReporter(trivialReporter));
+  jasmineEnv.addReporter(reporter);
 
-  jasmineEnv.specFilter = function(spec) {
-    return trivialReporter.specFilter(spec);
-  };
+  if (isPhantom) {
+    // exporting this to window because phantomjs/ConsoleReporter is complaining otherwise
+    window.console_reporter = reporter;
+  } else {
+
+    jasmineEnv.addReporter(new jasmine.JUnitXmlReporter('', true, false));
+    // passing reporter so we can access the individual DOM nodes
+    jasmineEnv.addReporter(new jasmine.CompareReporter(reporter));
+
+    jasmineEnv.specFilter = function(spec) {
+      return reporter.specFilter(spec);
+    };
+  }
 
   function execJasmine() {
     jasmineEnv.execute();
