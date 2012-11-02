9c62b872776afd656e2214ef71652f7942ce4bb6
Wolfram Kriesing <wk@uxebu.com>
2012-11-02 18:31:38 +0100

    Enhanced an example to have a use case.

--- example/library/movies/event-click.js
+++ example/library/movies/event-click.js
@@ -1,9 +1,7 @@
 /**
- * event click
- *
  * Path w/ events.
  */
-var rectPath = new Rect(150, 150, 150, 150).attr({fillColor: 'red'});
+var rectPath = bonsai.Path.rect(150, 150, 150, 150).attr({fillColor: 'red'});
 var colors = [
   'green',
   'blue',
@@ -15,6 +13,15 @@ var index = 0;
 stage.addChild(rectPath);
 
 rectPath.on('click', function(data){
+  incrementNumClicks();
   if (++index == colors.length) index = 0;
   rectPath.attr({fillColor: colors[index]});
 });
+
+var t = new Text().attr({x: 100, y: 10});
+t.addTo(stage);
+
+var numClicks = 0;
+function incrementNumClicks() {
+  t.attr({text: 'Number of clicks: ' + (numClicks++)});
+}
