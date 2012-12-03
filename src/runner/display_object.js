c26e43705f70da70fa7fd9db0985a4991604aee8
David Aurelio <david@uxebu.com>
2012-12-03 11:29:45 +0100

    Revert "Merge branch 'master' into transforms-to-be-set-by-matrix"
    This reverts commit fa681b8bcf485f1e25fe4ab30e77a60b5b7e621f, reversing
    changes made to a0b914c5c618e42920248f1e5a9c6a116dc309da.

--- src/runner/display_object.js
+++ src/runner/display_object.js
@@ -88,10 +88,10 @@ define([
 
   function setMatrix(matrix) {
     var m = this._matrix;
-    m.a = this._scaleX = matrix.a;
+    m.a = matrix.a;
     m.b = matrix.b;
     m.c = matrix.c;
-    m.d = this._scaleY = matrix.d;
+    m.d = matrix.d;
     m.tx = matrix.tx;
     m.ty = matrix.ty;
   }
