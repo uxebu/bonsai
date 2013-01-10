b3a0e3a26bfbdde5cdf510036783614dcd1836b3
Dustan Kasten <dustan.kasten@gmail.com>
2013-01-10 10:14:50 -0500

    Update demo movie

--- example/library/movies/shape-dashedstrokes.js
+++ example/library/movies/shape-dashedstrokes.js
@@ -1,8 +1,8 @@
 var rect = new Rect(50, 50, 100, 100).addTo(stage);
 rect.attr({
   strokeWidth: 1,
-  strokeDashArray: '5,4',
-  strokeOffset: '5',
+  strokeDash: [5, 4],
+  strokeDashOffset: '5',
   strokeColor: 'black',
   fillColor: 'yellow'
 });
@@ -12,10 +12,23 @@ var offset = false;
 setInterval(function () {
   i++;
 
-  rect.attr('strokeOffset', i);
+  rect.attr('strokeDashOffset', i);
 
   if (i === 9) i = 0;
 }, 100);
 
-}, 1000);
+var star = new Star(100, 97, 20, 5, 3).addTo(stage);
+star.attr({
+  strokeWidth: 1,
+  strokeDash: [10, 5, 10, 3],
+  strokeDashOffset: 5,
+  strokeColor: 'orange',
+  fillColor: 'white'
+});
+
+
+star.animate('1s', {
+  strokeDashOffset: 28
+}, { repeat: Infinity });
+
 
