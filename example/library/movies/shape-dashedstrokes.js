649307c207868839ad9c6c08a2fc052527ea9d1b
Dustan Kasten <dustan.kasten@gmail.com>
2013-01-15 09:11:35 -0500

    Shape morphing with strokeDash

--- example/library/movies/shape-dashedstrokes.js
+++ example/library/movies/shape-dashedstrokes.js
@@ -28,3 +28,25 @@ star.animate(new KeyframeAnimation('4s', {
 }, { repeat: Infinity }));
 
 
+// Base morph from animation-morph.js
+var a = Path.star(100, 100, 100, 5).attr({
+  x: 400, y: 400,
+  fillColor: 'red',
+  opacity: 0.5,
+  strokeDash: [20, 5, 10, 5],
+  strokeColor: 'blue',
+  strokeWidth: 5,
+  rotation: Math.PI
+});
+var b = new Star(250, 250, 200, 10).attr({
+  fillColor: 'blue',
+  strokeDash: [10, 10, 10, 10],
+  strokeColor: 'blue',
+  strokeWidth: 5,
+  rotation: 0
+});
+
+a.addTo(stage, 0);
+
+a.morphTo(b, '3s');
+
