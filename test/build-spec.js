eeffd189f39aedc41b6f5a4699f7417700931ba5
Tobias von Klipstein <klipstein@uxebu.com>
2013-01-24 01:16:14 +0100

    Refactor tests

--- test/build-spec.js
+++ test/build-spec.js
@@ -3,8 +3,10 @@ define(function() {
   describe('Bonsai build', function(){
     var dummyNode = document.getElementById('dummyNode');
 
+    var retBonsaiRun;
     var stage;
     beforeEach(function() {
+      retBonsaiRun = null;
       // bonsai namespace is filled up after first invocation
       stage = bonsai.run(dummyNode, { code: '1;'});
     });
@@ -12,7 +14,15 @@ define(function() {
       stage.destroy();
     });
 
-    bonsai.run(dummyNode, {});
+    var bonsaiRun = function(runObj) {
+      stage = bonsai.run(dummyNode, runObj);
+      stage.on('message:success', function(data) {
+        retBonsaiRun = data;
+      });
+      waitsFor(function() {
+        return !!retBonsaiRun;
+      }, '"bonsai.run" execution failed', 1000);
+    };
 
     describe('bonsai', function() {
 
@@ -59,38 +69,41 @@ define(function() {
 
       describe('run', function() {
         it('executes bonsai-code passed within "code" option', function() {
-          var loaded = false;
-          var retData;
-          stage = bonsai.run(dummyNode, {
-            code: 'stage.sendMessage("success", {foo: 10});'
+          bonsaiRun({
+            any: 'data',
+            code: 'stage.sendMessage("success", { foo: 10 });'
           });
-          stage.on('message:success', function(data) {
-            loaded = true;
-            retData = data;
-          });
-          waitsFor(function() {
-            return loaded;
-          }, '"bonsai.run" execution failed', 1000);
           runs(function() {
-            expect(retData.foo).toBe(10);
+            expect(retBonsaiRun.foo).toEqual(10);
           });
         });
-        it('allows to load requirejs through "urls"', function() {
-          var retData;
-          stage = bonsai.run(dummyNode, {
+        it('can load requirejs through "urls"', function() {
+          bonsaiRun({
             urls: ['../lib/requirejs/require.js'],
             // require + define can't be serialized in worker
             code: 'stage.sendMessage("success", {require: typeof require, define: typeof define});'
           });
-          stage.on('message:success', function(data) {
-            retData = data;
-          });
-          waitsFor(function() {
-            return !!retData;
-          }, '"bonsai.run" execution failed', 1000);
           runs(function() {
-            expect(retData.require).toBe('function');
-            expect(retData.define).toBe('function');
+            expect(retBonsaiRun.require).toBe('function');
+            expect(retBonsaiRun.define).toBe('function');
+          });
+        });
+        it('sets the framerate through "framerate"', function() {
+          bonsaiRun({
+            framerate: 10,
+            code: 'stage.sendMessage("success", {framerate: stage.framerate});'
+          });
+          runs(function() {
+            expect(retBonsaiRun.framerate).toBe(10);
+          });
+        });
+        it('allows to pass additional data', function() {
+          bonsaiRun({
+            any: 'data',
+            code: 'stage.sendMessage("success", { options: stage.options });'
+          });
+          runs(function() {
+            expect(retBonsaiRun.options.any).toEqual('data');
           });
         });
       });
