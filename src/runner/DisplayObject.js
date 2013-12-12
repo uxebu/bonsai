af9e4ab68511ca35d8ca1379bcdacaf6b81577d5
David Aurelio <david@uxebu.com>
2013-12-12 18:11:59 +0100

    Finish new attribute system

--- src/runner/DisplayObject.js
+++ src/runner/DisplayObject.js
@@ -2,7 +2,65 @@ define(function() {
   'use strict';
 
   function DisplayObject() {
+    this._attributes = {};
+  }
 
+  DisplayObject.prototype = {
+    attr: function(name, value) {
+      var key;
+      var attributes = this._attributes;
+      var numArguments = arguments.length;
+
+      if (numArguments === 0) { // return an object with all values
+        var copy = {};
+        for (key in attributes) {
+          if (startsWith(key, 'set_')) continue; // skip over setters
+          if (startsWith(key, 'get_')) {
+            key = key.slice(4);
+            if (key in attributes) continue; // only invoke getters if no attribute is found
+          }
+
+          copy[key] = getAttribute(attributes, key, this);
+        }
+
+        return copy;
+      }
+
+      if (numArguments === 1 && typeof name === 'object') { // update attributes from object
+        var values = name;
+        for (key in values) {
+          setAttribute(attributes, key, values[key], this);
+        }
+        return this;
+      }
+
+      if (numArguments === 2) { // set a single attribute
+        setAttribute(attributes, name, value, this);
+        return this;
+      }
+
+      return getAttribute(attributes, name, this);
+    }
+  };
+
+  function startsWith(string, prefix) {
+    return string.lastIndexOf(prefix, 0) === 0;
+  }
+
+  function setAttribute(attributes, name, value, owner) {
+    var setterName = 'set_' + name;
+    if (setterName in attributes) {
+      value = attributes[setterName](value, attributes[name], owner);
+    }
+    if (name in attributes) {
+      attributes[name] = value;
+    }
+  }
+
+  function getAttribute(attributes, name, owner) {
+    var getterName = 'get_' + name;
+    return getterName in attributes ?
+      attributes[getterName](attributes[name], owner) : attributes[name];
   }
 
   return DisplayObject;
--- test/unit/runner/DisplayObject.js
+++ test/unit/runner/DisplayObject.js
@@ -1,9 +1,132 @@
-define(['bdd', 'expect', 'bonsai/runner/DisplayObject'], function(bdd, expect, DisplayObject) {
-  var describe = bdd.describe, it = bdd.it;
+define([
+  'bdd',
+  'expect',
+  'sinon',
+  'bonsai/runner/DisplayObject'
+], function(bdd, expect, sinon, DisplayObject) {
+  var beforeEach = bdd.beforeEach, describe = bdd.describe, it = bdd.it;
 
   describe('DisplayObject', function() {
-    it('should fail', function() {
-      expect(true).to.equal(true);
+
+    describe('Attribute system:', function() {
+      function setAttributes(attributes) {
+        displayObject._attributes = attributes;
+      }
+      var displayObject;
+      beforeEach(function() {
+        displayObject = new DisplayObject();
+      });
+
+      it('gets an existing attribute', function() {
+        setAttributes({arbitrary: 1234});
+        expect(displayObject.attr('arbitrary')).to.equal(1234);
+      });
+
+      it('sets an existing attribute', function() {
+        setAttributes({arbitrary: undefined});
+        displayObject.attr('arbitrary', 1234);
+        expect(displayObject.attr('arbitrary')).to.equal(1234);
+      });
+
+      it('returns the display object when setting', function() {
+        expect(displayObject.attr('arbitrary', 1234)).to.equal(displayObject);
+      });
+
+      it('does not set non-existant attributes', function() {
+        setAttributes({});
+        displayObject.attr('arbitrary', 1234);
+        expect(displayObject.attr('arbitrary')).to.equal(undefined);
+      });
+
+      it('returns the value of existing getters', function() {
+        setAttributes({
+          arbitrary: 1234,
+          get_arbitrary: function() { return 5678; }
+        });
+        expect(displayObject.attr('arbitrary')).to.equal(5678);
+      });
+
+      it('uses the return value of existing setters to set the value of the attribute', function() {
+        setAttributes({
+          arbitrary: undefined,
+          set_arbitrary: function() { return 1234; }
+        });
+        displayObject.attr('arbitrary', 5678);
+        expect(displayObject.attr('arbitrary')).to.equal(1234);
+      });
+
+      it('invokes setters without assigning the value if the attribute name does not exist', function() {
+        var setArbitrary = sinon.spy(function() {
+          return 5678;
+        });
+        setAttributes({
+          set_arbitrary: setArbitrary
+        });
+        displayObject.attr('arbitrary', 1234);
+
+        expect(setArbitrary).to.have.been.called;
+        expect(displayObject.attr('arbitrary')).to.equal(undefined);
+      });
+
+      it('passes the current value and the display object to getters', function() {
+        var getArbitrary = sinon.spy();
+        setAttributes({
+          arbitrary: 1234,
+          get_arbitrary: getArbitrary
+        });
+
+        displayObject.attr('arbitrary');
+        expect(getArbitrary).to.have.been.calledWith(1234, displayObject);
+      });
+
+      it('passes the new value, the old value, and the display object to setters', function() {
+        var setArbitrary = sinon.spy();
+        setAttributes({
+          arbitrary: 1234,
+          set_arbitrary: setArbitrary
+        });
+
+        displayObject.attr('arbitrary', 5678);
+        expect(setArbitrary).to.have.been.calledWith(5678, 1234, displayObject);
+      });
+
+      it('returns an object containing all attributes when called without arguments', function() {
+        setAttributes({
+          foo: 1,
+          bar: 2,
+          get_bar: function(value) { return value + 2; },
+          get_baz: function() { return 3; }
+        });
+
+        expect(displayObject.attr()).to.deep.equal({
+          foo: 1,
+          bar: 4,
+          baz: 3
+        });
+      });
+
+      it('updates attributes from a passed-in object as single parameter', function() {
+        setAttributes({
+          foo: undefined,
+          bar: undefined,
+          set_bar: function(value) { return value + 2; },
+          baz: undefined,
+          set_bazoong: function(value) { this.baz = value; }
+        });
+
+        displayObject.attr({
+          foo: 1,
+          bar: 2,
+          bazoong: 3
+        });
+
+        expect(displayObject.attr()).to.deep.equal({
+          foo: 1,
+          bar: 4,
+          baz: 3
+        });
+      });
     });
+
   });
 });
