5a5deeca11000e7d694e423b92ae14e319d0cce6
David Aurelio <david@uxebu.com>
2013-12-12 11:49:25 +0100

    Add a source and a test

---
+++ test/runner/DisplayObject-test.js
@@ -0,0 +1,7 @@
+define(['bonsai/runner/DisplayObject'], function(DisplayObject) {
+  describe('DisplayObject', function() {
+    it('should fail', function() {
+      expect(false).to.equal(true);
+    });
+  });
+});
