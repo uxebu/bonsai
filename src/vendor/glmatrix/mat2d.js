9bec1c063ce1aed9ceb9ccb139a7148212655311
David Aurelio <david@uxebu.com>
2013-12-13 14:45:53 +0100

    Uncomment mat2d.copy

--- src/vendor/glmatrix/mat2d.js
+++ src/vendor/glmatrix/mat2d.js
@@ -77,22 +77,22 @@ define(function() {
 //    return out;
 //  };
 
-//  /**
-//   * Copy the values from one mat2d to another
-//   *
-//   * @param {mat2d} out the receiving matrix
-//   * @param {mat2d} a the source matrix
-//   * @returns {mat2d} out
-//   */
-//  mat2d.copy = function(out, a) {
-//    out[0] = a[0];
-//    out[1] = a[1];
-//    out[2] = a[2];
-//    out[3] = a[3];
-//    out[4] = a[4];
-//    out[5] = a[5];
-//    return out;
-//  };
+  /**
+   * Copy the values from one mat2d to another
+   *
+   * @param {mat2d} out the receiving matrix
+   * @param {mat2d} a the source matrix
+   * @returns {mat2d} out
+   */
+  mat2d.copy = function(out, a) {
+    out[0] = a[0];
+    out[1] = a[1];
+    out[2] = a[2];
+    out[3] = a[3];
+    out[4] = a[4];
+    out[5] = a[5];
+    return out;
+  };
 
 //  /**
 //   * Set a mat2d to the identity matrix
